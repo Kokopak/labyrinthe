199fad77782be96a2235d013c2ab4b2cc84368ec
Coco <corentin.chataignon44@gmail.com>
2014-11-01 00:44:52 +0100

    Implémentation de l'algo génération Laby + ajout gui pour voir le Laby.

---
+++ gui.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import pygame
+from pygame.locals import *
+from maze import Maze
+
+SIZE_CELL = 30
+
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+
+class Gui:
+
+    def __init__(self, rows, cols):
+        pygame.init()
+
+        self.rows = rows
+        self.cols = cols
+
+        self.maze = Maze(self.rows, self.cols)
+        self.maze.generate()
+        self.screen = pygame.display.set_mode((self.cols * SIZE_CELL, self.rows*SIZE_CELL))
+
+        pygame.display.set_caption("OnlineByrinth")
+
+        self.quit = False
+
+
+    def update(self):
+        return
+
+    def draw(self):
+        self.screen.fill((WHITE))
+        for pos, cell in self.maze.grid.items():
+            row, col = pos
+            row, col = row * SIZE_CELL, col * SIZE_CELL
+            if cell.n_wall:
+                pygame.draw.line(self.screen, RED, (col, row), (col + SIZE_CELL, row))
+            if cell.s_wall:
+                pygame.draw.line(self.screen, RED, (col, row + SIZE_CELL), (col + SIZE_CELL, row + SIZE_CELL))
+            if cell.w_wall:
+                pygame.draw.line(self.screen, RED, (col, row), (col, row + SIZE_CELL))
+            if cell.e_wall:
+                pygame.draw.line(self.screen, RED, (col + SIZE_CELL, row), (col + SIZE_CELL, row + SIZE_CELL))
+
+
+
+        pygame.display.flip()
+
+    def main_loop(self):
+        while not self.quit:
+            for event in pygame.event.get():
+                if event.type == QUIT:
+                    self.quit = True
+            self.update()
+            self.draw()
+
+
+if __name__ == '__main__':
+    gui = Gui(20, 50)
+    gui.main_loop()
---
+++ maze.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import random
+
+N = (-1, 0)
+S = (1, 0)
+E = (0, 1)
+W = (0, -1)
+
+class Cell:
+
+    def __init__(self, row, col):
+        self.init_walls()
+        self.row = row
+        self.col = col
+
+    def init_walls(self):
+        self.n_wall = True
+        self.s_wall = True
+        self.w_wall = True
+        self.e_wall = True
+
+    def all_walls(self):
+        return self.n_wall and self.s_wall and self.e_wall and self.w_wall
+
+    def __repr__(self):
+        return "<%d, %d>" % (self.row, self.col)
+
+class Maze:
+
+    def __init__(self, rows, cols):
+        self.rows = rows
+        self.cols = cols
+
+        self.grid = {}
+
+        for r in range(self.rows):
+            for c in range(self.cols):
+                self.grid[(r, c)] = Cell(r, c)
+
+    def generate(self):
+        cell_stack = []
+        total_cells = self.rows * self.cols
+        current_cell = self.grid[(0, 0)]
+        visited_cells = 1
+        while visited_cells < total_cells :
+            neighbors = self.find_neigh(current_cell)
+            if len(neighbors) >= 1:
+                neigh, direction = random.choice(neighbors)
+                if direction == N:
+                    current_cell.n_wall = False
+                    neigh.s_wall = False
+                elif direction == S:
+                    current_cell.s_wall = False
+                    neigh.n_wall = False
+                elif direction == E:
+                    current_cell.e_wall = False
+                    neigh.w_wall = False
+                elif direction == W:
+                    current_cell.w_wall = False
+                    neigh.e_wall = False
+                cell_stack.append(current_cell)
+                current_cell = neigh
+                visited_cells += 1
+            else:
+                current_cell = cell_stack.pop()
+
+
+    def find_neigh(self, cell):
+        neigh = [N, S, E, W]
+        neighbors = []
+        for n in neigh:
+            tmp_row, tmp_col = cell.row + n[0], cell.col + n[1]
+            if tmp_row >= 0 and tmp_col >= 0 and tmp_row < self.rows and tmp_col < self.cols:
+                tmp_neigh = self.grid[(tmp_row, tmp_col)]
+                if tmp_neigh.all_walls() :
+                    neighbors.append((tmp_neigh, n))
+
+        return neighbors
+
+
+if __name__ == '__main__':
+    maze = Maze(10, 30)
+    maze.generate()
